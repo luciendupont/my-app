529e2f5e6a31104d1a19d3d638cf02bca6b97043
luciendupont <lucienluludupont@gmail.com>
2023-11-29 16:17:36 +0100

    stude react app

---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+export default function Menu(){
+    return(
+        <div className="menu">
+        <ul>
+            <li>Home</li>
+            <li>Add Techno</li>
+            <li>ALL Technos</li>
+        </ul>
+
+        </div>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+export default function Home(){
+    return(
+        <div className="home">
+        <h1>My Progession</h1>
+        <div>Welcome to this site devoted to your programming progression</div>
+
+        </div>
+    )
+}
