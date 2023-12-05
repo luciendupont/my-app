7817def57c8531018305d434d8d5dbef709c4104
luciendupont <lucienluludupont@gmail.com>
2023-12-05 14:24:25 +0100

    cli

--- src/components/TechnoItem.js
+++ src/components/TechnoItem.js
@@ -1,12 +1,19 @@
-export  default function  TechnoItem(props){
-    const {techno} = props;
-    return (
-    <div key={techno.technoid}>
-    <h2>{techno.technoname}</h2>
-    <h3>Category</h3>
-    <p>{techno.technocategory}</p>
-    <h3>Description</h3>
-    <p>{techno.technodescription}</p>
-  </div>
+import React from "react";
+
+export default function TechnoItem(props) {
+  const { techno, handleDeleteTechno } = props;
+  return (
+    <>
+      <div key={techno.technoid} className="card">
+        <h2>{techno.technoname}</h2>
+        <h3>Category</h3>
+        <p>{techno.technocategory}</p>
+        <h3>Description</h3>
+        <p>{techno.technodescription}</p>
+        <div className="footer">
+          <button className="btn-delete" onClick={() => handleDeleteTechno(techno.technoid)}>delete</button>
+        </div>
+      </div>
+    </>
   );
 }
